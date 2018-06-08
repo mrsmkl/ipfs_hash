545e7bad63c7791aea01b976a3ce5fbaad2a6c13
Sami Mäkelä <mrsmkl@gmail.com>
2018-06-08 13:41:15 +0300

    working on more IPFS features

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate quick_protobuf;
 extern crate multihash;
+extern crate base58;
 
 use multihash::{encode, Hash};
 
@@ -36,6 +37,11 @@ fn concat_links<'a>(lst : &Vec<PBLink<'a>>) -> PBLink<'a> {
     PBLink{Tsize: Some(total_length(lst)), Name:None, Hash:Some(Cow::from(hash))}
 }
 
+fn hash_block(vec : &Vec<u8>) -> String {
+    let hash = encode(Hash::SHA2256, vec).unwrap();
+    base58::ToBase58::to_base58(hash.as_slice())
+}
+
 fn ipfs_tree(vec : &[u8]) -> PBLink {
     if vec.len() <= 1024 {
        data_link(vec)
@@ -53,9 +59,41 @@ fn ipfs_tree(vec : &[u8]) -> PBLink {
     }
 }
 
+// use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
+fn read_file(filename : &str) -> Vec<u8> {
+    println!("In file {}", filename);
+
+    let mut f = File::open(filename).expect("file not found");
+
+    let mut buffer = vec![];
+    // read the whole file
+    f.read_to_end(&mut buffer).expect("Blah");
+    
+    buffer
+
+}
+
+use quick_protobuf::{MessageRead, BytesReader};
+
+fn process_block(dta : &Vec<u8>) {
+    let mut reader = BytesReader::from_bytes(&dta);
+    let nd = proto::ipfs::PBNode::from_reader(&mut reader, &dta).expect("Cannot read node");
+    println!("Perhaps it will work {:?}", nd);
+    for l in nd.Links {
+       println!("Got link {:?}", base58::ToBase58::to_base58(l.Hash.unwrap().to_mut().as_slice()))
+    }
+}
+
 fn main() {
+    let dta = read_file("ipfsblock");
+    process_block(&dta);
     // use proto::ipfs::PBNode::*;
     let crump : &[u8] = "asassasasa".as_bytes();
+    let hash = encode(Hash::SHA2256, &crump.to_vec()).unwrap();
+    println!("Hashed {:?}", hash);
     let v = Cow::Borrowed(crump);
     let nd = proto::ipfs::PBNode{Links:[].to_vec(), Data:Some(v)};
     let mut out = Vec::new();
@@ -64,7 +102,9 @@ fn main() {
         writer.write_message(&nd).expect("Cannot write message!");
     }
     println!("Serialized {:?} to {:?}", nd, out);
-    println!("Make link {:?}", ipfs_tree("asasassaas".as_bytes()));
+    let res = ipfs_tree("asasassaas".as_bytes());
+    println!("Make link {:?}", res);
+    println!("Hashing it {:?}", hash_block(& "dsdsdsdssdsdsdds".as_bytes().to_vec()));
 }
 
 
